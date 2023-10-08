93178db8d86414a0366c6189dcbb5debcf3d4fcc
lymilal8 <129526576+lymilal8@users.noreply.github.com>
2023-10-08 18:36:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movie_ticket_booking
+ 
---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+var cors = require('cors');
+
+app.use(morgan('dev'));
+require('dotenv').config();
+require("./db/mongodb")
+app.use(cors());
+
+const api=require("./routes/movieRoute");
+app.use("/api",api);
+
+// const loginRouter = require("./routes/loginRoute");
+// app.use("/api",loginRouter)
+
+const userRouter = require("./routes/userRoute");
+app.use("/api",userRouter);
+
+const bookingRouter = require("./routes/bookingRoutes");
+app.use("/api",bookingRouter);
+
+
+
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`server listening on ${PORT}`);
+});
+
